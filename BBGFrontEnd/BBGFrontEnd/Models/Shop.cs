55db29338a59b05495cf0c5f5cd05d3b88f219c4
marc <1440411korver@zuyd.nl>
2020-01-25 21:33:17 +0100

    Added Shop model

---
+++ BBGFrontEnd/BBGFrontEnd/Models/Shop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BBGFrontEnd.Models
+{
+    public class Shop
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+        public int Phone { get; set; }
+        public DateTime OpenTime { get; set; }
+        public DateTime CloseTime { get; set; }
+        public List<bool> OpenDays { get; set; } // Lijst met 7 elementen die staan voor de dagen van de week.
+    }
+}
